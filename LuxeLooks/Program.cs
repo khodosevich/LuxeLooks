85feeb48e51b32215c358e635c41155722a66794
Alexey-beercode <alsemkovbn@gmail.com>
2023-09-15 16:55:29 +0300

    Refactor 2.0

--- LuxeLooks/Program.cs
+++ LuxeLooks/Program.cs
@@ -1,14 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
