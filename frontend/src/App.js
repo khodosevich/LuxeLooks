c9014a3e3eab8b53ebab2d0c6c14902ea4358a79
Matvey <hodos.952795@gmail.com>
2023-09-21 12:07:09 +0300

    init react project

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+        <div className="container">
+            hello , LUXE LOOKS
+        </div>
+
+
+    </div>
+  );
+}
+
+export default App;
