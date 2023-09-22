d96991a9e15acee346aa706c4c34b16b5c4b3c44
Matvey <hodos.952795@gmail.com>
2023-09-22 15:33:04 +0300

    add header component

---
+++ frontend/src/components/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Box} from "@mui/material";
+
+
+const Home = () => {
+    return (
+        <Box sx={{
+            background:"green",
+            width:"200px",
+            height:"200px"
+        }}>
+            home
+        </Box>
+    );
+};
+
+export default Home;
