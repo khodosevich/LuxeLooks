78f9260c1eb57fc335e4bd69c7c94e037d6049d7
Alexey-beercode <alsemkovbn@gmail.com>
2023-09-22 09:11:56 +0300

    Added JWT autorization and deleted identity

---
+++ LuxeLooks/LuxeLooks.Domain/Entity/User.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LuxeLooks.Domain.Entity;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string UserName { get; set; } = null!;
+    public string NormalizedUserName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string PasswordHash { get; set; } = null!;
+    public Guid RoleId { get; set; } = new Guid("44546e06-8719-4ad8-b88a-f271ae9d6eab");
+    public string? RefreshToken { get; set; }
+    public DateTime RefreshTokenExpiryTime { get; set; }
+    public string PasswordSalt { get; set; }
+
+}
---
+++ LuxeLooks/LuxeLooks/Controllers/Account/AccountController.cs
@@ -0,0 +1,175 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Net;
+using DwellEase.Domain.Models.Identity;
+using LuxeLooks.Domain.Entity;
+using LuxeLooks.Domain.Models.Identity;
+using LuxeLooks.Domain.Response;
+using LuxeLooks.Service.Extensions;
+using LuxeLooks.Service.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LuxeLooks.Controllers.Account;
+
+[ApiController]
+[Route("Account")]
+public class AccountController : ControllerBase
+{
+    private readonly TokenService _tokenService;
+    private readonly IConfiguration _configuration;
+    private readonly UserService _userService;
+    private readonly ILogger<AccountController>_logger;
+
+    public AccountController(TokenService tokenService, IConfiguration configuration, UserService userService, ILogger<AccountController> logger)
+    {
+        _tokenService = tokenService;
+        _configuration = configuration;
+        _userService = userService;
+        _logger = logger;
+    }
+    
+    [HttpPost("Login")]
+    public async Task<ActionResult<AuthResponse>> Authenticate([FromBody] AuthRequest request)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var managedUserResponse = await _userService.FindByNameAsync(request.UserName);
+        
+        if (managedUserResponse.StatusCode!=HttpStatusCode.OK)
+        {
+            return BadRequest("Bad credentials");
+        }
+
+        var managedUser = managedUserResponse.Data;
+        var isPasswordValidResponse = await _userService.CheckPasswordAsync(managedUser, request.Password);
+        
+        if (isPasswordValidResponse.StatusCode!=HttpStatusCode.OK)
+        {
+            return BadRequest("Bad credentials");
+        }
+
+        var userResponse = await _userService.FindByNameAsync(request.UserName);
+        
+        if (userResponse.StatusCode!= HttpStatusCode.OK)
+            return Unauthorized();
+
+        var user = userResponse.Data;
+        var accessToken =await _tokenService.CreateToken(user);
+        user.RefreshToken = _configuration.GenerateRefreshToken();
+        user.RefreshTokenExpiryTime = DateTime.UtcNow.AddDays(_configuration.GetSection("Jwt:RefreshTokenValidityInDays").Get<int>());
+        await _userService.UpdateAsync(user);
+        
+        return Ok(new AuthResponse
+        {
+            Username = user.UserName!,
+            Email = user.Email!,
+            Token = accessToken,
+            RefreshToken = user.RefreshToken
+        });
+    }
+    
+    [HttpPost("Register")]
+    public async Task<ActionResult<AuthResponse>> Register( RegisterRequest request)
+    {
+        if (!ModelState.IsValid) return BadRequest(request);
+        var user = new User()
+        {
+            Email = request.Email,
+            UserName = request.UserName,
+        };
+        var result = await _userService.CreateAsync(user, request.Password);
+
+        if (result.StatusCode!=HttpStatusCode.OK) return BadRequest(ModelState.Values);
+
+        var findUserResponse = await _userService.FindByNameAsync(request.UserName);
+
+        if (findUserResponse.StatusCode !=HttpStatusCode.OK) throw new Exception($"User {request.UserName} not found");
+            
+        return await Authenticate(new AuthRequest
+        {
+            UserName = request.UserName,
+            Password = request.Password
+        });
+    }
+    
+    [HttpPost("Refresh-Token")]
+    public async Task<IActionResult> RefreshToken(TokenModel? tokenModel)
+    {
+        if (tokenModel is null || string.IsNullOrEmpty(tokenModel.AccessToken))
+        {
+            return BadRequest("Invalid client request");
+        }
+
+        var accessToken = tokenModel.AccessToken;
+        var refreshToken = tokenModel.RefreshToken;
+        var principal = _configuration.GetPrincipalFromExpiredToken(accessToken);
+        
+        if (principal == null)
+        {
+            return BadRequest("Invalid access token or refresh token");
+        }
+        
+        var username = principal.Identity!.Name;
+        var userResponse = await _userService.FindByNameAsync(username!);
+        if (userResponse.StatusCode!=HttpStatusCode.OK)
+        {
+            return BadRequest();
+        }
+
+        var user = userResponse.Data;
+        if ( user.RefreshToken != refreshToken || user.RefreshTokenExpiryTime <= DateTime.UtcNow)
+        {
+            return BadRequest("Invalid access token or refresh token");
+        }
+
+        var newAccessToken = _configuration.CreateToken(principal.Claims.ToList());
+        var newRefreshToken = _configuration.GenerateRefreshToken();
+
+        user.RefreshToken = newRefreshToken;
+        await _userService.UpdateAsync(user);
+
+        return new ObjectResult(new
+        {
+            accessToken = new JwtSecurityTokenHandler().WriteToken(newAccessToken),
+            refreshToken = newRefreshToken
+        });
+    }
+    
+    [Authorize]
+    [HttpPost]
+    [Route("Revoke/{username}")]
+    public async Task<IActionResult> Revoke(string username)
+    {
+        var response = await _userService.FindByNameAsync(username);
+        if (response.StatusCode!=HttpStatusCode.OK) return BadRequest("Invalid user name");
+        var user = response.Data;
+        user.RefreshToken = null;
+        await _userService.UpdateAsync(user);
+
+        return Ok();
+    }
+    
+    [Authorize]
+    [HttpPost]
+    [Route("Revoke-all")]
+    public async Task<IActionResult> RevokeAll()
+    {
+        var response =await _userService.GetAllAsync();
+        if (response.StatusCode!=HttpStatusCode.OK)
+        {
+            return NoContent();
+        }
+
+        var users = response.Data;
+        foreach (var user in users)
+        {
+            user.RefreshToken = null;
+            await _userService.UpdateAsync(user);
+        }
+
+        return Ok();
+    }
+}
