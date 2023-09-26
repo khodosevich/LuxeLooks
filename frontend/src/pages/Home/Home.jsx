7c0e30a507bcbbdb74729005330c4474a5320033
Matvey <hodos.952795@gmail.com>
2023-09-26 16:40:25 +0300

    add categories component to frontend

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Landing from "../../components/Landing/Landing";
+import TrendingNow from "../../components/trendingnow/TrendingNow";
+import Categories from "../../components/categories/Categories";
 
 const Home = () => {
     return (
        <>
         <Landing/>
+         <Categories/>
+        <TrendingNow/>
        </>
     );
 };
