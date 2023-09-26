710ae846f7693db187f1c41cdf1e09558e322abd
Matvey <hodos.952795@gmail.com>
2023-09-26 20:59:02 +0300

    clean frontend branch

--- LuxeLooks/LuxeLooks.Domain/Models/Identity/AuthResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace LuxeLooks.Domain.Models.Identity;
-
-public class AuthResponse
-{
-    public string Username { get; set; } = null!;
-    public string Email { get; set; } = null!;
-    public string Token { get; set; } = null!;
-    public string RefreshToken { get; set; } = null!;
-}
