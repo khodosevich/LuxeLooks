85c680b8a4cfcb4af2561d2157928e2fa0c843a6
Alexey-beercode <alsemkovbn@gmail.com>
2024-01-29 16:14:50 +0300

    Added entity, migration, repository for reviews

--- LuxeLooks/LuxeLooks.DataManagment/Migrations/20240129131338_Added_Reviews.Designer.cs
+++ LuxeLooks/LuxeLooks.DataManagment/Migrations/20240129131338_Added_Reviews.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using LuxeLooks.DataManagment;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace LuxeLooks.DataManagment.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240129131338_Added_Reviews")]
+    partial class Added_Reviews
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ LuxeLooks/LuxeLooks.DataManagment/Repositories/Implementations/ReviewRepository.cs
@@ -0,0 +1,39 @@
+﻿using LuxeLooks.DataManagment.Repositories.Interfaces;
+using LuxeLooks.Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace LuxeLooks.DataManagment.Repositories.Implementations;
+
+public class ReviewRepository:IBaseRepository<Review>
+{
+    private readonly ApplicationDbContext _db;
+
+    public ReviewRepository(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    public async Task Create(Review? entity)
+    {
+        await _db.Reviews.AddAsync(entity);
+        await _db.SaveChangesAsync();
+    }
+
+    public Task<List<Review>> GetAll()
+    {
+        return _db.Reviews.ToListAsync();
+    }
+
+    public async Task Delete(Review? entity)
+    {
+        _db.Reviews.Remove(entity);
+        await _db.SaveChangesAsync();
+    }
+
+    public async Task<Review> Update(Review entity)
+    {
+        _db.Reviews.Update(entity);
+        await _db.SaveChangesAsync();
+        return entity;
+    }
+}
