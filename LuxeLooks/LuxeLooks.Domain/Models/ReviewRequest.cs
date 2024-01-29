13be8aaab089461d9ea2d9713b5b9a35a368ad8d
Alexey-beercode <alsemkovbn@gmail.com>
2024-01-29 17:53:24 +0300

    Added mapper, validator, review controller

---
+++ LuxeLooks/LuxeLooks.Domain/Models/ReviewRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace LuxeLooks.Domain.Models;
+
+public class ReviewRequest
+{
+    public string UserId { get; set; }
+    public string Title { get; set; }
+    public string ProductId { get; set; }
+}
---
+++ LuxeLooks/LuxeLooks.Service/Services/ReviewService.cs
@@ -0,0 +1,55 @@
+﻿using LuxeLooks.DataManagment.Repositories.Interfaces;
+using LuxeLooks.Domain.Entity;
+using LuxeLooks.Domain.Models;
+using LuxeLooks.SharedLibrary;
+using LuxeLooks.SharedLibrary.Mappers;
+
+namespace LuxeLooks.Service.Services;
+
+public class ReviewService
+{
+    private readonly IBaseRepository<Review> _reviewRepository;
+    private readonly StringToGuidMapper _guidMapper;
+
+    public ReviewService(IBaseRepository<Review> reviewRepository, StringToGuidMapper guidMapper)
+    {
+        _reviewRepository = reviewRepository;
+        _guidMapper = guidMapper;
+    }
+
+    public async Task<List<Review>> GetByProductId(string id)
+    {
+        var productId = _guidMapper.MapTo(id);
+        var reviews = await _reviewRepository.GetAll();
+        if (reviews.Count==0)
+        {
+            throw new InvalidOperationException("Reviews are not found");
+        }
+
+        return reviews.Where(a => a.ProductId == productId).ToList();
+    }
+
+    public async Task Create(ReviewRequest reviewRequest)
+    {
+        var review = new Review()
+        {
+            CreateDate = DateTime.Now,
+            ProductId = _guidMapper.MapTo(reviewRequest.ProductId),
+            Title = reviewRequest.Title,
+            UserId = _guidMapper.MapTo(reviewRequest.UserId)
+        };
+        await _reviewRepository.Create(review);
+    }
+
+    public async Task<List<Review>> GetByUserId(string userId)
+    {
+        var guidUserId = _guidMapper.MapTo(userId);
+        var reviews = await _reviewRepository.GetAll();
+        if (reviews.Count==0)
+        {
+            throw new InvalidOperationException("Reviews are not found");
+        }
+
+        return reviews.Where(a => a.UserId == guidUserId).ToList();
+    }
+}
